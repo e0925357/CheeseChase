99a3fc3b7394534edc3a386ac5d86765a4e04e5e
Christoph Weiler <christoph.weiler@yahoo.de>
2015-03-24 15:06:41 +0100

    -pthreads added, gitignore improved

--- src/CheeseChase/main.cpp
+++ src/CheeseChase/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <thread>
+
 
 using namespace std;
 
+void foo() {
+    cout << "Hello World!" << endl;
+}
+
 int main()
 {
+    std::thread t1(foo);
     cout << "Hello World!" << endl;
+    t1.join();
     return 0;
 }
 
